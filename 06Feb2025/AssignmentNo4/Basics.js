50f83cdd14569c32cb9b87578bd0202cd149e897
Uzaifisani <uzaifisani8@gmail.com>
2025-02-06 18:38:12 +0530

    Assignment no 4 :Map(),Reduce(),Filter()

---
+++ 06Feb2025/AssignmentNo4/Basics.js
@@ -0,0 +1,33 @@
+const products = [
+  { name: "Laptop", price: 1000, category: "Electronics" },
+  { name: "Phone", price: 500, category: "Electronics" },
+  { name: "Shirt", price: 30, category: "Clothing" },
+  { name: "Shoes", price: 80, category: "Footwear" },
+];
+const ProductNameUppercase = products.map((product) => {
+  return product.name.toUpperCase();
+});
+console.log(ProductNameUppercase);
+
+const ElectronicProducts = products.filter((products) => {
+  return products.category === "Electronics";
+});
+console.log(ElectronicProducts);
+
+const TotalPrice = products.reduce((acc, product) => {
+  return acc + product.price;
+}, 0);
+console.log("Total of All item is ", TotalPrice);
+
+const calculateCategoryTotal = (category) => {
+  return products
+    .filter((product) => product.category === category)
+    .map((product) => product.price)
+    .reduce((acc, price) => acc + price, 0);
+};
+
+console.log(
+  "Total price of Electronics:",
+  calculateCategoryTotal("Electronics")
+);
+console.log("Total price of Clothing:", calculateCategoryTotal("Clothing")); // Should output 30
